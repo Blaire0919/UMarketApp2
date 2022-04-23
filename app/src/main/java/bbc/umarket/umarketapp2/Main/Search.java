c6578819bc5cc52db0289de6f61f8324b5bbf991
Rowgie <rowgietulipas@gmail.com>
2022-04-23 22:26:49 +0800

    Search Feature applied with bugs

--- app/src/main/java/bbc/umarket/umarketapp2/Main/Search.java
+++ app/src/main/java/bbc/umarket/umarketapp2/Main/Search.java
@@ -1,21 +1,24 @@
 package bbc.umarket.umarketapp2.Main;
 
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,25 +28,26 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 import bbc.umarket.umarketapp2.Adapter.ItemAdapter;
-import bbc.umarket.umarketapp2.Helper.FeatProdtHelperClass;
 import bbc.umarket.umarketapp2.Helper.ItemHelperClass;
 import bbc.umarket.umarketapp2.R;
 
 public class Search extends AppCompatActivity {
 
     ImageView back;
-    DatabaseReference mref;
+    DatabaseReference prodref;
 
     private ListView listdata;
     private AutoCompleteTextView txtSearch;
 
     RecyclerView searchrv;
     ArrayList<ItemHelperClass> searched_item;
+//    ArrayList<ItemHelperClass> newSearch;
     ItemAdapter itemAdapter;
 
     @Override
@@ -53,7 +57,7 @@ public class Search extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //hide status bar
 
 //        Database Reference on Products
-        mref = FirebaseDatabase.getInstance("https://umarketapp2-58178-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        prodref = FirebaseDatabase.getInstance("https://umarketapp2-58178-default-rtdb.asia-southeast1.firebasedatabase.app/")
                 .getReference("products");
 
 //        Hooks
@@ -62,18 +66,29 @@ public class Search extends AppCompatActivity {
         back = (ImageView) findViewById(R.id.search_back);
         searchrv = (RecyclerView) findViewById(R.id.search_recyclerview);
 
+        //for filtered recyclerview
+        //for product listing
+        searchrv.setHasFixedSize(true);
+        searchrv.setLayoutManager(new StaggeredGridLayoutManager(3, LinearLayoutManager.VERTICAL));
+        searched_item = new ArrayList<>();
+        itemAdapter = new ItemAdapter(this, searched_item);
+        searchrv.setAdapter(itemAdapter);
+
+
 
         ValueEventListener event = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 populateSearch(snapshot);
+
+
             }
 
             @Override
 
             public void onCancelled( DatabaseError error) {  }
         };
-        mref.addListenerForSingleValueEvent(event);
+        prodref.addListenerForSingleValueEvent(event);
 
         back.setOnClickListener(view -> {
             Intent intent = new Intent(Search.this, HomeContainer.class);
@@ -90,22 +105,16 @@ public class Search extends AppCompatActivity {
 //            return false;
 //        });
 
+
+
     }
 
 //    Methods
     private void populateSearch(DataSnapshot snapshot) {
         ArrayList<String> pnames = new ArrayList<>();
 
-        //for filtered recyclerview
-        //for product listing
-        searchrv.setHasFixedSize(true);
-        searchrv.setLayoutManager(new StaggeredGridLayoutManager(3, LinearLayoutManager.VERTICAL));
-        searched_item = new ArrayList<>();
-        itemAdapter = new ItemAdapter(this, searched_item);
-        searchrv.setAdapter(itemAdapter);
-
+//        Auto Suggest
         if(snapshot.exists()){
-
             for(DataSnapshot ds:snapshot.getChildren()){
                 String pname = ds.child("pName").getValue(String.class);
                 pnames.add(pname);
@@ -119,8 +128,71 @@ public class Search extends AppCompatActivity {
             ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, pnames);
             txtSearch.setAdapter(adapter);
 
-        }else{Log.d("Users", "No data found");
+
+//            Clicking suggestion function
+            txtSearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                    String name = txtSearch.getText().toString();
+                    searchProduct(name);
+                }
+            });
+
+            txtSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                    if (i == EditorInfo.IME_ACTION_SEARCH){
+                        String search_query = textView.getText().toString();
+                        searchProduct(search_query);
+
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
+        }else{Log.d("Products", "No data found");
         }
+    }
+
+
+    private void searchProduct(String name){
+        txtSearch.clearFocus();
+
+        InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        in.hideSoftInputFromWindow(txtSearch.getWindowToken(), 0);
+
+        String searchInputToLower  = name.toLowerCase();
+        String searchInputToUpper  = name.toUpperCase();
+
+        Query query = prodref.orderByChild("pName").startAt(name).endAt(name + "\uf8ff");
+//        Query query = prodref.orderByChild("pName").equalTo(name);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if(snapshot.exists()){
+                    searched_item.clear();
+                    itemAdapter.notifyDataSetChanged();
+
+                    for(DataSnapshot ds : snapshot.getChildren()){
+                        ItemHelperClass itemHelperClass = ds.getValue(ItemHelperClass.class);
+                        searched_item.add(itemHelperClass);
+                    }
+                    itemAdapter = new ItemAdapter(getApplicationContext(), searched_item);
+                    searchrv.setAdapter(itemAdapter);
+                    itemAdapter.notifyDataSetChanged();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
 
